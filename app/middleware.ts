ca4b3497c2a4cd6297b608f16c700441492772a1
StevenRosaa <giacomo.palmisani.tablet@gmail.com>
2025-11-17 18:30:46 +0100

    Rifiniture nel middleware e grafica.

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,8 +1,10 @@
 import { type NextRequest } from 'next/server'
 import { createClient } from '@/lib/supabase/middleware'
 
+
 export async function middleware(request: NextRequest) {
   // Questo chiama l'helper che abbiamo appena sistemato
+  console.log("MW: Middleware eseguito su:", request.nextUrl.pathname)
   const { supabase, response } = createClient(request)
 
   // getUser() forza il refresh dei cookie se necessario
