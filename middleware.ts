469b33a97a628eb4d90c2ff74161d4b707b334b8
StevenRosaa <giacomo.palmisani.tablet@gmail.com>
2025-11-17 18:37:34 +0100

    Rifiniture nel middleware e grafica.

--- middleware.ts
+++ middleware.ts
@@ -16,6 +16,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|auth/callback|manifest.webmanifest|manifest.json|icon-.*png).*)',
   ],
 }
